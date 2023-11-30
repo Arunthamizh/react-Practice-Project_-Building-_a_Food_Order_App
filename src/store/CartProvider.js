677d7f5249303d45660d9771a6ab49b9394c29cb
“Arunthamizh” <arunthamizh.azhagiri@twilightitsolutions.com>
2023-11-30 20:12:40 +0530

    14th-video adding cart reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,34 +1,54 @@
-import React from "react";
+import React, { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const cartReducer = (state, action) => {
+    if(action.type === 'ADD'){
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.price * action.amount
 
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    } 
+
+    if(action.type === 'REMOVE'){
+
+    }
+
+  return defaultCartState;
+};
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
 // * CartProvider components is simply a wrapper around the CartContext component
 // * CartProvider manage the cartContext data and provide it to the all the components that need to access it
 
-const CartProvider =  props => {
-    const addItemCartHandler =  (item) => {
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+  const addItemCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
 
-     }
+  const removeItemFormCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
-     const removeItemCartHandler = (id) => {
+  const cartContext = {
+    item: [],
+    totalAmount: 0,
+    AddItem: addItemCartHandler,
+    RemoveIte: removeItemFormCartHandler,
+  };
 
-     }
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
 
-
-     const cartContext  = {
-        item: [],
-        totalAmount: 0,
-        AddItem: addItemCartHandler,
-        RemoveIte: removeItemCartHandler,
-    }
-
-    return(
-        <CartContext.Provider value={cartContext}>
-            {props.children}
-        </CartContext.Provider>
-    )
-}
-
-
-export default CartProvider
+export default CartProvider;
