486a1e203c90d9af0dbbcc7285ec9f6c39894d95
Arunthamizh-laptop <workattwilight@gmail.com>
2023-12-02 17:18:51 +0530

    17th-video outputting cart and remove the duplicate item added

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -10,7 +10,26 @@ const defaultCartState = {
 // ! The action contains new state values and action type that are passed in the reducer
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
+    // ! check if the item is already in the cart
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    // let updatedItem;
+    let updatedItems;
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      // ! update the item in the cart
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      // updatedItem = { ...action.item };
+      updatedItems = state.items.concat(action.item);
+    }
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
