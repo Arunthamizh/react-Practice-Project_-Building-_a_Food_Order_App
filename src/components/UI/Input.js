3b23f9c3cd04081bb059ae65a9714566ed34b35d
Arunthamizh-laptop <workattwilight@gmail.com>
2023-12-02 15:21:34 +0530

    add comments

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,19 +1,20 @@
-
-import React from 'react';
-import classes from './Input.module.css';
+import React from "react";
+import classes from "./Input.module.css";
 // ! React.forwardRef() => this means now the component function is argument to React.forwardRef()
+// !  this means now the component function is argument to React.forwardRef()
+// ! Now we get that ref as second argument to the component function and we forward it to the input element
 const Input = React.forwardRef((props, ref) => {
-    return (
-        <div className={classes.input}>
-            <label htmlFor={props.input.id}>{props.label}</label>
-            <input ref={ref} {...props.input} />
-            {/* // ! This is the shorthand for setting a props for an input element */}
-            {/* {...props.input}
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input ref={ref} {...props.input} />
+      {/* // ! This is the shorthand for setting a props for an input element */}
+      {/* {...props.input}
                 This is the shorthand for setting a props for an input element
                 instead of this: type={props.input.type} id={props.input.id} value={props.input.value} 
             */}
-        </div>  
-    );
-})
+    </div>
+  );
+});
 
 export default Input;
--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+// ! The CartContext component is simply a wrapper around the CartContext component
+// ! The CreteContext component is used to create a context
+// ! The CartContext component manage the cartContext data and provide it to the all the components that need to access it
 const CartContext = React.createContext({
   items: [],
   totalAmount: 0,
